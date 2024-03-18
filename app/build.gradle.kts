64476878a14b60adba78fb5c340541e6fc32fdfb
rick <givemepassxd999@gmail.com>
2024-03-18 11:56:41 +0800

    feat: 加入koin retrofit paging等 lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,14 +36,51 @@ android {
     buildFeatures {
         viewBinding = true
     }
+
 }
 
-dependencies {
 
+
+dependencies {
+    ext {
+        set("paging_version", "3.3.0-alpha04")
+        set("kotlin_version", "3.3.0-alpha04")
+        set("retrofit_version", "2.9.0")
+        set("retrofit_log_version", "4.9.3")
+        set("lifecycle_version", "2.7.0")
+        set("refresh_version", "1.1.0")
+        set("koin_version", "3.1.6")
+    }
+
+    // Koin Core features
+    implementation("io.insert-koin:koin-core:${ext.get("koin_version")}")
+    // Koin main features for Android
+    implementation("io.insert-koin:koin-android:${ext.get("koin_version")}")
+    // Java Compatibility
+    implementation("io.insert-koin:koin-android-compat:${ext.get("koin_version")}")
+
+    // retrofit
+    implementation("com.squareup.retrofit2:retrofit:${ext.get("retrofit_version")}")
+    implementation("com.squareup.retrofit2:converter-gson:${ext.get("retrofit_version")}")
+    implementation("com.squareup.retrofit2:retrofit-mock:${ext.get("retrofit_version")}")
+    implementation("com.squareup.okhttp3:logging-interceptor:${ext.get("retrofit_log_version")}")
+
+    // lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${ext.get("lifecycle_version")}")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${ext.get("lifecycle_version")}")
+
+    // paging
+    implementation("androidx.paging:paging-runtime-ktx:${ext.get("paging_version")}")
+
+    // swiperefreshlayout
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:${ext.get("refresh_version")}")
+
+    // architecture components
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
